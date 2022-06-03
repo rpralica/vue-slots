50fd0eaa9a1dfdda6739f7051f2acc944b58cada
Rade <rpralica@gmail.com>
2022-06-03 12:20:48 +0200

    Novi Commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <my-buttons>
+      <template v-slot:header>
+        <h2>Header</h2>
+      </template>
+
+      <template v-slot:body>
+        <div>
+          <h2>Body</h2>
+        </div>
+      </template>
+
+      <template v-slot:footer>
+        <div>
+          <h2>Footer</h2>
+        </div>
+      </template>
+    </my-buttons>
+  </div>
+</template>
+
+<script>
+import MyButtons from './components/MyButtons.vue';
+export default {
+  components: {
+    MyButtons,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/MyButtons.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <slot name="header"></slot>
+  </div>
+  <br>
+  <div>
+    <slot name="body"></slot>
+  </div>
+  <br>
+  <div>
+    <slot name="footer"></slot>
+  </div>
+</template>
+
+<style>
+div {
+  background: blue;
+  color: white;
+}
+</style>
